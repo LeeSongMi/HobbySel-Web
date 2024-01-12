a5fb2e541394109c617f2f9ac6393d8ef90ae408
leesongmi <iridescentpine@gmail.com>
2024-01-12 15:24:22 +0900

    ✨ Create Main, Counseling, Result Page - 메인 페이지 작업 - 카운슬링 페이지 생성 - 결과 페이지 생성

---
+++ src/app/result/page.tsx
@@ -0,0 +1,14 @@
+export default function ResultPage() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-10">
+      <h1 className="text-black">Tendency Type</h1>
+      <div className="text-wrap text-center">
+        How about trying a static hobby? <br />
+        When it comes to hobbies, <br />
+        you tend to value experiences more than actual material things. <br />
+        A solution for those who want to develop their artistic sense! <br />
+        Start your favorite hobby among the hobbies below!
+      </div>
+    </div>
+  );
+}
